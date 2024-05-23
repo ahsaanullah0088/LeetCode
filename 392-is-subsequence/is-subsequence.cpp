2c65db747c66a387f8b97a9c643cbeb096793104
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2024-05-23 18:18:42 +0500

    Time: 3 ms (46.81%) | Memory: 7.5 MB (30.39%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i =0;  
-        int j=0;
-        while(i<s.size() && j<t.size()){
-            if(t[j] == s[i]){
-                i++;
+        int i = 0;  // Pointer for string s
+        int j = 0;  // Pointer for string t
+
+        // Traverse both strings
+        while (i < s.size() && j < t.size()) {
+            if (t[j] == s[i]) {
+                i++;  // Move to the next character in s
             }
-            j++;
+            j++;  // Move to the next character in t regardless of match
         }
-        if(i==s.size()){
-            return true;
-        }
-        else{
-            return false;
-        }
+
+        // If we've matched all characters in s, return true
+        return i == s.size();
     }
 };
