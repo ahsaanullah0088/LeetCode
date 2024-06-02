a140bbad4bec3478db69fe44a2c88d0df963c9b0
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2024-06-02 16:01:31 +0500

    Time: 520 ms (5.08%) | Memory: 19.3 MB (57.91%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -13,8 +13,6 @@ public:
                 return nums[i];
             }
         }
-        // If the loop completes without finding a single number, there may be an issue with the input data.
-        // You may want to handle this case differently depending on the requirements.
-        return -1; // Or any other suitable value indicating an error or no single number found
+        return -1; 
     }
 };
