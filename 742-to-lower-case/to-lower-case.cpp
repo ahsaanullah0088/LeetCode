246ec8a4f7f79ab0e7d006d845a3c66d8de91a9d
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2024-06-15 18:08:14 +0500

    Time: 3 ms (43.96%) | Memory: 7.3 MB (65.31%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
            for (auto &c : s) {
-        c = tolower(static_cast<unsigned char>(c));
+        c = tolower(c);
     } 
             return s;
     }
