bcd1486f43028453447ab16ef0f90996910bb10e
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2024-07-17 23:05:40 +0500

    Time: 4 ms (35.14%) | Memory: 9.9 MB (30.20%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -1,13 +1,12 @@
-#include<algorithm>
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-     int max = 0;
-     int curr=  0; 
-     for(  int i=0; i< gain.size(); i++){
-        curr += gain[i];
-        max = std::max(curr , max);
-     }
-     return max;
-}
+        int ans=0; 
+        int hi =0;
+        for(int i=0; i<gain.size(); i++){
+         ans += gain[i];
+         hi=max(hi, ans);
+        }
+        return hi;
+    }
 };
