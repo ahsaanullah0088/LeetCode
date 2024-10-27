43003091d5b322ce55da06f79f29b3c4a4203208
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2024-10-27 02:13:26 -0700

    Time: 0 ms (100.00%) | Memory: 18.2 MB (12.39%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-       int sum =0;
-       for(int i=0; i<operations.size(); i++){
-        if(operations[i].at(1) == '+') sum++;
-        else sum--;
-       }
-       return sum;
+        int ans =0;
+        for(int i=0; i<operations.size();i++){
+            if(operations[i] == "++X" || operations[i] ==  "X++"){
+                ++ans;
+        }else{
+            ans--;
+        }
+    }
+    return ans;
     }
 };
