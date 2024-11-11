86fd937edb3421cb03b26896499a0bc9e40d1d2e
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2024-11-11 12:08:58 -0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.50%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> m1, m2;
-        if(s.size() !=t.size()){
+        vector<int> indexS(200, 0); // Stores index of characters in string s
+        vector<int> indexT(200, 0); // Stores index of characters in string t
+        
+        int len = s.length(); // Get the length of both strings
+        
+        if(len != t.length()) { // If the lengths of the two strings are different, they can't be isomorphic
             return false;
         }
-        for(int i=0; i<s.size(); i++){
-            if(m1.find(s[i]) != m1.end()){
-                if(m1[s[i]]!= t[i]){
-                    return false;
-                }
-            } else if(m2.find(t[i])!=m2.end()){
-                if(m2[t[i]] != s[i]){
-                    return false;
-                }
-            }else{
-                m1[s[i]] = t[i];
-                m2[t[i]] = s[i];
+        
+        for(int i = 0; i < len; i++) { // Iterate through each character of the strings
+            if(indexS[s[i]] != indexT[t[i]]) { // Check if the index of the current character in string s is different from the index of the corresponding character in string t
+                return false; // If different, strings are not isomorphic
             }
+            
+            indexS[s[i]] = i + 1; // updating position of current character
+            indexT[t[i]] = i + 1;
         }
-        return true;
+        
+        return true; // If the loop completes without returning false, strings are isomorphic
     }
 };
