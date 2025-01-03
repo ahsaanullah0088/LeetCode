ff2232ec16851a9ccbfd367ff71946fa7eb79529
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-03 19:55:08 +0500

    Time: 7 ms (29.53%) | Memory: 9.5 MB (71.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-   bool isAnagram(string s, string t) {
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-    return(s==t);
-}
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s==t){
+            return true;
+        }
+        return false;
+    }
 };
