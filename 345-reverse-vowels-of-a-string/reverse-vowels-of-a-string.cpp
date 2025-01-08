51a0a1df5b5f6d41831b479a0aaa749b61221cd2
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-08 11:52:18 +0500

    Time: 0 ms (100.00%) | Memory: 10.4 MB (50.30%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,23 +1,20 @@
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-bool checkVovels(char c){
-    return(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'|| c=='A'|| c=='E'|| c=='I'|| c=='O'|| c=='U');
+bool CheckVovels(char c){
+    return(
+        c=='a'|| c=='e' || c=='i'||c=='o'||c=='u'||c=='A'|| c=='E' || c=='I'||c=='O'||c=='U'
+    );
 }
     string reverseVowels(string s) {
-        int left =0, right = s.size()-1;
-        while(left< right){
-            if(! checkVovels(s[left])){
-                left ++;
-            }
-            else if(! checkVovels(s[right])){
-                right --;
-            }
-            else{
-                swap(s[left] , s[right]);
-                left ++;
+        int left=0, right=s.size()-1;
+        while(left<right){
+            if(!CheckVovels(s[left])){
+                left++;
+            } else if(!CheckVovels(s[right])){
+                right--;
+            }else{
+                swap(s[left], s[right]);
+                left++;
                 right--;
             }
         }
