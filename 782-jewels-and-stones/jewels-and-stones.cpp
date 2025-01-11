2800c9a1193438fb174981c54ede6f4b0c57be14
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-11 17:48:29 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (31.72%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-    unordered_set<char> jewelSet;
-    for (char jewel : jewels) {
-        jewelSet.insert(jewel);
-    }
-
-    // Find total score/value
-    int score = 0;
-    for (char stone : stones) {
-        if (jewelSet.count(stone)) {
-            score++;
+        unordered_set<char> jewelSet;
+        for (char jewel : jewels) {
+            jewelSet.insert(jewel);
         }
-    }
 
-    return score;
+        // Find total score/value
+        int score = 0;
+        for (int i = 0; i < stones.size(); i++) {
+            if (jewelSet.find(stones[i]) != jewelSet.end()) {
+                score++;
+            }
+        }
+
+        return score;
     }
 };
