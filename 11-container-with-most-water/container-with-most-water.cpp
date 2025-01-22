f24b7f254faaa9ee8a38ca69ae8020bcfaccdb71
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-22 12:05:10 +0500

    Time: 0 ms (100.00%) | Memory: 63.1 MB (26.18%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-    int left = 0;                // Start pointer
-    int right = height.size() - 1; // End pointer
-    int maxWater = 0;            // Store the maximum water
-
-    while (left < right) {
-        // Calculate the area
-        int width = right - left;
-        int minHeight = min(height[left], height[right]);
-        int currentArea = width * minHeight;
-
-        // Update the maximum water area
-        maxWater = max(maxWater, currentArea);
-
-        // Move the pointer corresponding to the smaller height
-        if (height[left] < height[right]) {
-            left++;
-        } else {
-            right--;
+        int left =0; 
+        int right = height.size() -1;
+        int maxarea =0;
+        while(left<right){
+            int widht = right - left;
+            int minheight  = min(height[left] , height[right]);
+            int currentArea = widht * minheight;
+            maxarea = max( maxarea , currentArea);
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
         }
-    }
-
-    return maxWater;
+        return maxarea;
     }
 };
