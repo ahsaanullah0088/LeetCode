273d0f3d21425a3b72d45b5db43ba101bd1e5cd0
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-27 07:19:18 +0500

    Time: 1 ms (16.47%) | Memory: 9.9 MB (5.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    bool checkValid(char opening, char closing) {
-        return ((opening == '(' && closing == ')') ||
-                (opening == '[' && closing == ']') ||
-                (opening == '{' && closing == '}'));
-    }
-
     bool isValid(string s) {
-        stack<char> stack;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-                stack.push(s[i]);
-            } else {
-                if (stack.empty()) {
-                    return false;
-                }
-                if (checkValid(stack.top(), s[i])) {
-                    stack.pop();
-                } else {
+        stack<int> st;
+        for( int i=0; i<s.size(); i++ ){
+            char temp = s[i];
+            if(temp == '(' ||temp == '{' ||temp == '['){
+                st.push(temp);
+            }else{
+                if(!st.empty() &&  ((temp == ')' && st.top() == '(' ) ||
+                                     (temp == '}' && st.top() == '{') ||
+                                     (temp == ']' && st.top() == '[' ))){
+                st.pop();
+                }else{
                     return false;
                 }
             }
         }
-        return stack.empty();
+        return st.empty();
     }
 };
