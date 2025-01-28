3783849779f6cd12154e36075142bd727b4cf687
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-28 21:37:46 +0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.53%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     string makeGood(string s) {
         string result = "";
-        for(char &ch : s) {
-            if(!result.empty() && (result.back() - 32 == ch || result.back() + 32 == ch)) {
-                result.pop_back(); 
-            } else {
-                result.push_back(ch); 
+        for( char &ch : s){
+            if(!result.empty() && ( result.back() -32 == ch || result.back()+32 == ch)){
+                result.pop_back();
+            }else{
+                result.push_back(ch);
             }
         }
         return result;
