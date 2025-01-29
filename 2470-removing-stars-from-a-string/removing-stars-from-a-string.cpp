4e880a0abf7a6f217ecaca66b21107d5689df965
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-01-29 21:31:33 +0500

    Time: 14 ms (75.96%) | Memory: 28.4 MB (72.60%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -3,7 +3,7 @@ public:
     string removeStars(string s) {
         string stack;
         for(char ch : s){
-            if(!stack.empty() && ch == '*' ){
+            if(!stack.empty() && ch == '*'){
                 stack.pop_back();
             }else{
                 stack.push_back(ch);
