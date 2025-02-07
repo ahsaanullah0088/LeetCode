9aa1079702f08f0cb539f4274fd0701a874dd76d
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-02-07 21:13:33 +0500

    Time: 13 ms (31.90%) | Memory: 7.6 MB (98.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        //bottom up DP
-        //creating the dp array
-        vector<int>dp(n+2);
-        //VVI::here i have used vector of n+2 because in case of n=0 dp[1] will give error otherwise in generic way i Should write an if condition after dp[0] that if n==0 then return dp[0];
-        //putting the base cases
-        dp[0]=0;
-        dp[1]=1;
-        //fill rest of the dp array with the help of recursive relation i.e dp[n]=dp[n-1]+dp[n-2]
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if (n <= 1) {
+            return n;
         }
-        return dp[n];
-        
-     }
+        return fib(n - 1) + fib(n - 2); // No memoization!
+    }
 };
