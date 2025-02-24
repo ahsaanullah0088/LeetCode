4376a54a987be09118cccd5d20aa3e1ba453fc0a
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-02-24 15:55:39 +0500

    Time: 7 ms (22.63%) | Memory: 10.5 MB (11.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -6,14 +6,12 @@ public:
         }
         unordered_map<char , int> counts , countT;
         for(int i=0; i<s.size();i++){
-            counts[s[i]] = 1+counts[s[i]];
-            countT[t[i]] = 1+countT[t[i]];
-
+            counts[s[i]] ++;
+            countT[t[i]] ++;
         }
-        for(auto & pair : counts){
-            if(countT[pair.first] != pair.second){
-                return false;
-            }
+
+        for(int i=0; i<counts.size(); i++){
+            if(counts[i] != countT[i]) return false;
         }
         return true;
     }
