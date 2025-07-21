8100d1662efe950d8c34938009ba4cbb9fa253e0
Ahsaan Ullah <124255630+ahsaanullah0088@users.noreply.github.com>
2025-07-21 22:52:19 +0500

    Time: 6 ms (20.56%) | Memory: 19 MB (19.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> count;
-        for(int num:nums){
-            count[num]++;
+        // I have declared a map here to put the value and increase the count
+        unordered_map<int, int> cnt;
+        for (int num : nums) {
+            cnt[num]++;
         }
-        vector<pair<int , int>> freq;
-        for(const auto& p:count){
-            freq.push_back({p.second, p.first});
+
+        // A new vector arr is declared 
+        vector<pair<int, int>> arr;
+        for (const auto& p : cnt) {
+            arr.push_back({p.second, p.first});
         }
-        sort(freq.rbegin(), freq.rend());
+
+        // Sort the vector in descending order of frequency
+        sort(arr.rbegin(), arr.rend());
+
+        // The result vector to store and return the answer
         vector<int> res;
-        for(int i=0; i<k; i++){
-            res.push_back(freq[i].second);
+        for (int i = 0; i < k; i++) {
+            res.push_back(arr[i].second);
         }
+
         return res;
     }
 };
